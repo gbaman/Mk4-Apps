093906132f1af64d34f16be1e2cf9071c4dcbc6f
gbaman <gbaman@users.noreply.github.com>
2018-09-01 19:47:19 +0100

    Add initial main

---
+++ light_painting/main.py
@@ -0,0 +1,7 @@
+""" starts an LED party on your badge
+"""
+___name___         = "LED light painting"
+___license___      = "MIT"
+___dependencies___ = ["wifi", "http", "ugfx_helper", "sleep"]
+___categories___   = ["LEDs"]
+___bootstrapped___ = False # Whether or not apps get downloaded on first install. Defaults to "False", mostly likely you won't have to use this at all.
